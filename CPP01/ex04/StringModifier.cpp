439f2389bdbf3f983aad6f78bd944838a567918e
AntoineCaffard <caffard.antoine@gmail.com>
2024-11-05 09:00:04 +0100

    optimized sed

--- CPP01/ex04/StringModifier.cpp
+++ CPP01/ex04/StringModifier.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   StringModifier.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:15:50 by acaffard          #+#    #+#             */
-/*   Updated: 2024/10/07 19:16:26 by acaffard         ###   ########.fr       */
+/*   Updated: 2024/11/05 08:58:53 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,25 @@ StringModifier::StringModifier(std::string base_string, std::string value_to_mod
 	_base_string(base_string), _to_find(value_to_modify), _new_value(new_value)
 {
 	_modified_string = "";
-	return ;
 }
 
 StringModifier::~StringModifier()
 {
-	return ;
+
 }
 
 int	StringModifier::modifyOnce()
 {
-	size_t	modify_index;
-	modify_index = _base_string.find(_to_find);
+	size_t modify_index = _base_string.find(_to_find);
 	if (modify_index == std::string::npos)
 	{
 		_modified_string.append(_base_string);
+		_base_string.clear();
 		return (-1);
 	}
-	_modified_string.append(_base_string.substr(0, modify_index));
+	_modified_string.append(_base_string, 0, modify_index);
 	_modified_string.append(_new_value);
-	_base_string = _base_string.substr(modify_index + _to_find.length());
+	_base_string = _base_string.erase(0, modify_index + _to_find.length());
 	return (0);
 }
 void	StringModifier::modify()

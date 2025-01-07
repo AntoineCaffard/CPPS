c8b76447addf181d9fdd8c779b1b0d4e209a5081
AntoineCaffard <caffard.antoine@gmail.com>
2025-01-07 08:06:27 +0100

    updated main cpp02-02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 13:56:41 by acaffard          #+#    #+#             */
-/*   Updated: 2025/01/06 11:44:35 by acaffard         ###   ########.fr       */
+/*   Updated: 2025/01/07 08:05:43 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,18 @@
 int main( void ) {
 Fixed a;
 Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+Fixed  c(3);
 std::cout << a << std::endl;
 std::cout << ++a << std::endl;
 std::cout << a << std::endl;
 std::cout << a++ << std::endl;
 std::cout << a << std::endl;
 std::cout << b << std::endl;
+a = b;
+std::cout << a + c << std::endl;
+std::cout << c - a << std::endl;
+std::cout << c * a << std::endl;
+std::cout << c / a << std::endl;
 std::cout << Fixed::max( a, b ) << std::endl;
+std::cout << Fixed::min( a, b ) << std::endl;
 }

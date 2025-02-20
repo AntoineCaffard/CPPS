e10ed78d97a923105460ac8467576ca19c19a804
AntoineCaffard <caffard.antoine@gmail.com>
2025-02-20 07:37:36 +0100

    Fixed Main

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acaffard <acaffard@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 12:32:50 by acaffard          #+#    #+#             */
-/*   Updated: 2025/02/12 13:44:38 by acaffard         ###   ########.fr       */
+/*   Updated: 2025/02/20 07:37:20 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,18 @@
 
 int main()
 {
-	// {
-	// 	try
-	// 	{
-	// 		Bureaucrat John("John", 10);
-	// 		// Bureaucrat Donald("Donald", -100);
-	// 		// Bureaucrat Random("Random", 250);
-
-	// 		// Bureaucrat Donald("Donald", 1);
-	// 		// Donald.IncreaseGrade();
-
-	// 		// Bureaucrat John("John", 10);
-	// 		// Bureaucrat Random("Random", 150);
-	// 		// Random.DecreaseGrade();
-
-	// 		Bureaucrat Random("Random", 149);
-	// 		Random.DecreaseGrade();
-	// 		John.IncreaseGrade();
-
-	// 		std::cout << John << Random;
-	// 		// std::cout << Donald << std::endl;
-	// 		// std::cout << Random << std::endl;
-	// 	}
-	// 	// catch(const std::exception& e)
-	// 	// {
-	// 	// 	std::cerr << e.what() << std::endl;
-	// 	// }
-	// }
 	{
 		AForm *wowo;
 		AForm *wawa;
 		AForm *wiwi;
+		AForm *wewe;
 		try
 		{
 			Intern Maurice;
 			wowo = Maurice.makeForm("robotomy request", "Michel");
 			wawa = Maurice.makeForm("presidential pardon", "Robert");
 			wiwi = Maurice.makeForm("shrubbery creation", "Jackie");
-			// AForm *wewe = Maurice.makeForm("shrubberyfadsfaasasd creation", "Jackie");
+			// wewe = Maurice.makeForm("shrubberyfadsfaasasd creation", "Jackie");
 			Bureaucrat John("John", 1);
 			Bureaucrat Random("Random", 149);
 			Random.signForm(*wowo);
@@ -69,7 +43,6 @@ int main()
 			John.executeForm(*wawa);
 			John.executeForm(*wowo);
 			John.executeForm(*wiwi);
-			// delete wewe;
 		}
 		catch(const std::exception& e)
 		{
@@ -78,5 +51,6 @@ int main()
 		delete wowo;
 		delete wawa;
 		delete wiwi;
+			// delete wewe;
 	}
 }

65e0716e166ede291a489e8e9d577f4ae923466a
AntoineCaffard <caffard.antoine@gmail.com>
2025-04-28 17:39:43 +0200

    Finished EX00 OSKUUUUR

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,7 +6,24 @@
 /*   By: acaffard <acaffard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:17:35 by acaffard          #+#    #+#             */
-/*   Updated: 2025/03/25 13:17:36 by acaffard         ###   ########.fr       */
+/*   Updated: 2025/04/28 17:16:49 by acaffard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int main (int ac, char **av)
+{
+	try
+	{
+		if (ac != 2)
+			throw BadArgumentNumber();
+		ScalarConverter::convert(av[1]);
+		return 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return 1;
+	}
+}

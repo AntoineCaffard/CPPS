b08643a82e03477299ef404340e2d3ed18e2d1a0
AntoineCaffard <caffard.antoine@gmail.com>
2025-09-10 17:27:28 +0000

    started jacob

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -22,7 +22,7 @@ class PmergeMe
     void sort_deque(int ac, char **av);
 
   private:
-    template <typename T> void _merge_insertion_sort(T& container, int pair_level);
+    void _merge_insertion_sort_vector(std::vector<int>& container, int pair_level);
     template <typename T> void _swap_pair(T it, int pair_level);
 };
 
@@ -44,65 +44,6 @@ template <typename T> void PmergeMe::_swap_pair(T it, int pair_level)
     {
         std::iter_swap(start, next(start, pair_level));
         start++;
-    }
-}
-
-template <typename T> void PmergeMe::_merge_insertion_sort(T& container, int pair_level)
-{
-    typedef typename T::iterator Iterator;
-
-    int pair_units_nbr = container.size() / pair_level;
-    if (pair_units_nbr < 2)
-        return;
-    bool is_odd = pair_units_nbr % 2 == 1;
-    Iterator start = container.begin();
-
-    Iterator last = next(container.begin(), pair_level * (pair_units_nbr));
-    Iterator end = next(last, -(is_odd * pair_level));
-
-    std::vector<Iterator> main;
-    std::vector<Iterator> pend;
-    std::vector<int> copy;
-
-    for (Iterator it = start; it != end; std::advance(it, 2 * pair_level))
-    {
-        Iterator this_pair = next(it, pair_level - 1);
-        Iterator next_pair = next(it, pair_level * 2 - 1);
-        if (_comp(next_pair, this_pair))
-        {
-            _swap_pair(this_pair, pair_level);
-        }
-    }
-    _merge_insertion_sort(container, pair_level * 2);
-    main.insert(main.end(), next(container.begin(), pair_level - 1));
-    main.insert(main.end(), next(container.begin(), pair_level * 2 - 1));
-    for (int i = 4; i <= pair_units_nbr; i += 2)
-    {
-        pend.insert(pend.end(), next(container.begin(), pair_level * (i - 1) - 1));
-        main.insert(main.end(), next(container.begin(), pair_level * i - 1));
-    }
-    if (is_odd)
-        pend.insert(pend.end(), next(end, pair_level - 1));
-
-    for (typename std::vector<Iterator>::iterator it = pend.begin(); it != pend.end(); it++)
-        main.insert(std::upper_bound(main.begin(),main.end(), *it, _comp<Iterator>), *it);
-
-    
-    for (typename std::vector<Iterator>::iterator it = main.begin(); it != main.end(); ++it)
-    {
-        Iterator pair_start = *it;
-        std::advance(pair_start, -(pair_level - 1));
-        for (int i = 0; i < pair_level; ++i, ++pair_start)
-            copy.push_back(*pair_start);
-    }
-
-    Iterator container_it = container.begin();
-    std::vector<int>::iterator copy_it = copy.begin();
-    while (copy_it != copy.end())
-    {
-        *container_it = *copy_it;
-        container_it++;
-        copy_it++;
     }
 }
 
